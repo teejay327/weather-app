8253d259ed50fc373ce197220f72ed0d578deeec
teejay327 <mrenergy@hotmail.com>
2023-01-01 23:39:10 +1000

    Changed date in app.js

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
 
   const forecast = {
     location: 'Gold Coast, Queensland',
-    date: new Date(2022, 11, 29),
+    date: new Date(2023, 0, 1),
     concise: 'Mostly sunny, light winds from the North East. Possible evening shower.',
     temperature: '12 to 26 degrees' 
   };
