b605509fdc9211ad2d450735bc8a767f1e66e281
teejay327 <mrenergy@hotmail.com>
2023-01-06 23:59:39 +1000

    UI changes

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -4,24 +4,37 @@ function Statistics (props) {
   return (
     <div className='statistics'>
       <div className='box'>
-        Chance of rain 
-        { props.chance }
+        <div className='stats__icon'>
+          Icon1
+        </div>
+        <div className='stats__text'>
+          Stats1
+        </div>
       </div>
-
       <div className='box'>
-        Rainfall 
-        { props.amount }
+        <div className='stats__icon'>
+        Icon2
+        </div>
+        <div className='stats__text'>
+        Stats2
+        </div>
       </div>
-
       <div className='box'>
-        UV Index 
-        { props.uv }
+        <div className='stats__icon'>
+        Icon3
+        </div>
+        <div className='stats__text'>
+        Stats3
+        </div>
       </div>
-
       <div className='box'>
-        Wind 
-        { props.wind }
+        <div className='stats__icon'>
+        Icon4     
+        </div>
+      <div className='stats__text'>
+      Stats4
       </div>
+     </div>
 
     </div>
   )
