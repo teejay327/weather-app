2f22adc95e253dafe099e268df5f6a3341055337
teejay327 <mrenergy@hotmail.com>
2023-01-16 00:47:53 +1000

    Changed to grid display

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,15 +1,17 @@
 import './Statistics.css';
-import rainfall from '../assets/Rainfall.png';
-import rainfallGauge from '../assets/RainfallGauge.png';
+//import rainfall from '../assets/Rainfall.png';
+//import rainfallGauge from '../assets/RainfallGauge.png';
 import uvIndex from '../assets/UVIndex.png';
 import wind from '../assets/Wind.png';
+import rain from '../assets/rainy-outline.svg';
 
 function Statistics (props) {
   return (
     <div className='statistics'>
       <div className='box'>
         <div className='stats__icon'>
-          <img src={ rainfall } alt="Rainfall" />
+          <img src={ rain } alt="Rainfall"/>
+          {/* <img src={ rainfall } alt="Rainfall"/> */}
         </div>
         <div className='stats__text'>
           Stats1
@@ -19,16 +21,17 @@ function Statistics (props) {
         <div className='stats__icon'>
 
         <svg width="40px" height="40px" viewBox="0 0 46 46">
-<g fill="none" fill-rule="evenodd"><circle fill="#53B9EA" cx="23" cy="23" r="23">
-</circle><g transform="translate(15 9)">
-<rect stroke="#FFF" stroke-width="1.7" x="0.85" y="0.85" width="15.3" height="26.118" rx="1"></rect>
-<path stroke="#FFF" stroke-width="1.7" stroke-linecap="round" d="M11 11.5h5M11 16.5h5M11 6.5h5"></path>
-<path d="M16 20.853c-.28.595-1.635.893-4.065.893C8.767 21.746 7.961 20 4.921 20c-1.316 0-2.623.284-3.921.853V27h15v-6.147z" fill="#FFF"></path>
-</g></g>
-</svg>
-
-
-          <img src={ rainfallGauge } alt="Rainfall Gauge" />
+          <g fill="none" fill-rule="evenodd">
+            <circle fill="#53B9EA" cx="23" cy="23" r="23"></circle>
+            <g transform="translate(15 9)">
+              <rect stroke="#FFF" stroke-width="1.7" x="0.85" y="0.85" width="15.3" height="26.118" rx="1"></rect>
+              <path stroke="#FFF" stroke-width="1.7" stroke-linecap="round" d="M11 11.5h5M11 16.5h5M11 6.5h5"></path>
+              <path d="M16 20.853c-.28.595-1.635.893-4.065.893C8.767 21.746 7.961 20 4.921 20c-1.316 0-2.623.284-3.921.853V27h15v-6.147z" fill="#FFF"></path>
+            </g>
+          </g>
+        </svg>
+          
+          {/* <img src={ rainfallGauge } alt="Rainfall Gauge" /> */}
         </div>
         <div className='stats__text'>
         Stats2
@@ -44,12 +47,12 @@ function Statistics (props) {
       </div>
       <div className='box'>
         <div className='stats__icon'>
-        <svg width="40px" height="40px" viewBox="0 0 46 46">
+{/*         <svg width="40px" height="40px" viewBox="0 0 46 46">
 <g fill="none" fill-rule="evenodd"><circle fill="#53B9EA" cx="23" cy="23" r="23"></circle>
 <path d="M20.05 34.353c2.62.899 5.534.424 7.707-1.256 2.913-2.21 4.004-5.996 2.695-9.347C30.077 22.263 27.485 11 27.485 11h0c-11.631 10.57-11.529 10.372-9.892 8.918-3.01 2.05-4.306 5.75-3.206 9.15.814 2.574 2.942 4.56 5.628 5.252l.034.033z" 
 stroke="#FFF" stroke-width="2" stroke-linejoin="round"></path></g>
 
-</svg>
+</svg> */}
           <img src={ wind } alt="Wind" />  
         </div>
       <div className='stats__text'>
