2751217c4e25a3598f33d58382069404edaffd35
teejay327 <mrenergy@hotmail.com>
2023-01-29 23:49:43 +1000

    Inputs for app.js added

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ import brisbaneRadar from './assets/Brisbane_radar_1.gif';
 function App() {
   const forecast = {
     location: 'Gold Coast, Queensland',
-    date: new Date(2023, 0, 27),
+    date: new Date(2023, 0, 29),
     concise: 'Mostly sunny, light winds from the North East. Possible evening shower.',
     temperature: '12 to 26 degrees' 
   };
 
   const stats = { 
-    chanceRain: '20%',
+    chanceRain: '50%',
     rainfall: '<1mm',
     uvIndex: 'Very high',
     wind: 'NE 20kmh'
