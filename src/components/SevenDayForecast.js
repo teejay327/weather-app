c8f6a24419ff3c13238fe6d8452d41181861919d
teejay327 <mrenergy@hotmail.com>
2023-02-04 23:28:28 +1000

    Added 7 day forecast component

---
+++ src/components/SevenDayForecast.js
@@ -0,0 +1,17 @@
+import './SevenDayForecast.css';
+
+function SevenDayForecast() {
+  return (
+    <div className='sevendayforecast'>
+      <div className='sevendayforecast__title'>
+        <h4>7 day forecast for </h4>
+      </div>
+
+      <div className='box'>
+        
+      </div>
+    </div>
+  )
+}
+
+export default SevenDayForecast;
