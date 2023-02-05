11a5e3b68e02be0ff03590fe691f18c544e2181c
teejay327 <mrenergy@hotmail.com>
2023-02-06 00:49:54 +1000

    modified UI seven day fprecast

--- src/components/SevenDayForecast.js
+++ src/components/SevenDayForecast.js
@@ -3,21 +3,30 @@ import './SevenDayForecast.css';
 function SevenDayForecast() {
   return (
     <div className='sevendayforecast'>
-      <div className='box'>
-        <div className='sevendayforecast__icon'>
-        
-        </div>
-        <div className='sevendayforecast__text'>
-          <div className='sevendayforecast__text-title'>
-
-          </div>
-          <div className='sevendayforecast__temperatures'>
-            
-          </div>
-          <div className='sevendayforecast__rainfall'>
-            
-            </div>
-        </div>
+      <div className='sevendayforecast__box'>
+       <ul>
+        <li>
+          <p>day1</p>
+        </li>
+        <li>
+          <p>day2</p>
+        </li>
+        <li>
+          <p>day3</p>
+        </li>
+        <li>
+          <p>day4</p>
+        </li>
+        <li>
+          <p>day5</p>
+        </li>
+        <li>
+          <p>day6</p>
+        </li>
+        <li>
+          <p>day7</p>
+        </li>
+       </ul>
       </div>
     </div>
   )
