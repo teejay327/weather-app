b8261d4aa293ea21d1ee348b42052f476355929f
teejay327 <mrenergy@hotmail.com>
2023-02-13 23:55:27 +1000

    fixed bugs with navbar and 7 day forecast

--- src/components/SevenDayForecast.js
+++ src/components/SevenDayForecast.js
@@ -9,20 +9,48 @@ function SevenDayForecast() {
        <ul>
         <li>
           <div>
-            <img src={wind} />
+            <img src={wind} alt='wind'/>
           </div>
-          <p>day1</p>
-          <p>day1</p>
-          <p>day1</p>
-          <p>day1</p>
-          <p>day5</p>
-         
 
+            <div className='sevendayforecast__date' >Tuesday 14 February</div>
+            <div className='sevendayforecast__mintemp' >21&#8451;</div>
+            <div className='sevendayforecast__maxtemp' >32&#8451;</div>
+            <div className='sevendayforecast__rainchance' >30%</div>
+            <div className='sevendayforecast__rainamount' >1-5mm</div>
         </li>
+
         <li>
-          <p>day2</p>
+          <div>
+            <img src={wind} alt='wind'/>
+          </div>
+            <div className='sevendayforecast__date' >Wednesday 15 February</div>
+            <div className='sevendayforecast__mintemp' >20&#8451;</div>
+            <div className='sevendayforecast__maxtemp' >31&#8451;</div>
+            <div className='sevendayforecast__rainchance' >40%</div>
+            <div className='sevendayforecast__rainamount' >1-5mm</div>
         </li>
 
+        <li>
+          <div>
+            <img src={wind} alt='wind'/>
+          </div>
+            <div className='sevendayforecast__date' >Thursday 16 February</div>
+            <div className='sevendayforecast__mintemp' >22&#8451;</div>
+            <div className='sevendayforecast__maxtemp' >30&#8451;</div>
+            <div className='sevendayforecast__rainchance' >40%</div>
+            <div className='sevendayforecast__rainamount' >1-5mm</div>
+        </li>
+
+        <li>
+          <div>
+            <img src={wind} alt='wind'/>
+          </div>
+            <div className='sevendayforecast__date' >Friday 17 February</div>
+            <div className='sevendayforecast__mintemp' >19&#8451;</div>
+            <div className='sevendayforecast__maxtemp' >28&#8451;</div>
+            <div className='sevendayforecast__rainchance' >20%</div>
+            <div className='sevendayforecast__rainamount' >1mm</div>
+        </li>
        </ul>
       </div>
     </div>
--- src/components/Warnings.js
+++ src/components/Warnings.js
@@ -4,7 +4,11 @@ function Warnings() {
   return (
     <div className="warnings" >
       <h3>Weather warnings for Gold Coast</h3>
+      <div className="warnings__text" >
+        <div>Heatwave conditions continuing over the next 48 hours in South East QLD</div>
+      </div>
     </div>
+
   )
 }
 
