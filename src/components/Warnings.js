f9ff1d8f18fa22608791df6773dd730011f75307
teejay327 <mrenergy@hotmail.com>
2023-03-07 23:51:02 +1000

    Added more to warnings file

--- src/components/Warnings.js
+++ src/components/Warnings.js
@@ -5,7 +5,9 @@ function Warnings() {
     <div className="warnings" >
       <h3>Weather warnings for Gold Coast</h3>
       <div className="warnings__text" >
-        <div>Heatwave conditions continuing over the next 48 hours in South East QLD</div>
+        <div>Heatwave conditions continuing over the next 48 hours in South East QLD
+          More rain on the way
+        </div>
       </div>
     </div>
 
