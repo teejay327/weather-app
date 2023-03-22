75503d9d59f7c8a9c16593cda4064e92a6f1b9ba
teejay327 <mrenergy@hotmail.com>
2023-03-22 23:44:14 +1000

    added chnage handler for form

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -2,20 +2,18 @@ import React, {useState} from 'react';
 import './SearchForm.css';
 
 function SearchForm(props) {
-  const [location, setLocation] = useState(props.location);
-  const clickHandler = () => {
-    // Dummy code
-    setLocation('location updated');
-    console.log(location);
+  const [newLocation, setLocation] = useState(props.location);
+  const locationClickHandler = (event) => {
+    setLocation(event.target.value);
+    console.log(newLocation);
   }
   return (
     <form>
       <div className='searchform'>
         <label>Search for any location</label>
-        <input type="text" placeholder="Gold Coast" />
-      
+        <input type="text" placeholder="Gold Coast" value={ newLocation }/>
       <div className='searchform__button'>
-        <button type="submit" className='button' onClick={ clickHandler }>Submit</button>
+        <button type="submit" className='button' onClick={ locationClickHandler }>Submit</button>
       </div>
       </div>
     </form>
