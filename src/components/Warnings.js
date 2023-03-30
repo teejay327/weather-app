43103bb1d7e7d4a8fc12ef33d573c591255620e9
teejay327 <mrenergy@hotmail.com>
2023-03-30 23:42:14 +1000

    Changed Warnings moduleW

--- src/components/Warnings.js
+++ src/components/Warnings.js
@@ -5,9 +5,7 @@ function Warnings() {
     <div className="warnings" >
       <h3>Weather warnings for Gold Coast</h3>
       <div className="warnings__text" >
-        <div>Heatwave conditions continuing over the next 48 hours in South East QLD
-          More rain on the way
-        </div>
+        <div>Dry conditions continuing over the next 48 hours in South East QLD</div>
       </div>
     </div>
 
