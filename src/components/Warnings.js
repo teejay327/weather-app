9c0555f4d86d9290d7bf361274702ce06a13ad58
teejay327 <mrenergy@hotmail.com>
2024-02-11 22:56:49 +1000

    Changed waether conditions

--- src/components/Warnings.js
+++ src/components/Warnings.js
@@ -5,7 +5,7 @@ function Warnings() {
     <div className="warnings" >
       <h3>Weather warnings for Gold Coast</h3>
       <div className="warnings__text" >
-        <div>Dry conditions continuing over the next 48 hours in South East QLD</div>
+        <div>Dry conditions continuing over the next 24 hours in South East QLD</div>
       </div>
     </div>
 
