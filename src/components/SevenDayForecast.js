68d9b9f6d85c496c3b4111c2ad9c0f3a350dfe08
teejay327 <mrenergy@hotmail.com>
2024-09-06 21:14:27 +1000

    Changed temperatures

--- src/components/SevenDayForecast.js
+++ src/components/SevenDayForecast.js
@@ -71,7 +71,7 @@ function SevenDayForecast() {
           </svg>
           </div>
             <div className='sevendayforecast__date' >Friday 9 August</div>
-            <div className='sevendayforecast__mintemp' >10&#8451;</div>
+            <div className='sevendayforecast__mintemp' >11&#8451;</div>
             <div className='sevendayforecast__maxtemp' >23&#8451;</div>
             <div className='sevendayforecast__rainchance' >10%</div>
             <div className='sevendayforecast__rainamount' >2mm</div>
