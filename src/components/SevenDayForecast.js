59238cff768186688e0193b8e21391602216a2f6
teejay327 <mrenergy@hotmail.com>
2024-09-30 23:38:06 +1000

    Changed inputs

--- src/components/SevenDayForecast.js
+++ src/components/SevenDayForecast.js
@@ -74,7 +74,7 @@ function SevenDayForecast() {
             <div className='sevendayforecast__mintemp' >11&#8451;</div>
             <div className='sevendayforecast__maxtemp' >24&#8451;</div>
             <div className='sevendayforecast__rainchance' >10%</div>
-            <div className='sevendayforecast__rainamount' >2mm</div>
+            <div className='sevendayforecast__rainamount' >3mm</div>
         </li>
 
         <li>
